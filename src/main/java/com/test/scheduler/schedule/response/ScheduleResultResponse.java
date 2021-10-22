694d28f296316754e238e4821cb2bd78151e4203
Valdery Alves <valdery.jur@gmail.com>
2021-10-22 03:23:16 -0300

    feat: Adds interface to map the result report of some schedule into a object
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- src/main/java/com/test/scheduler/schedule/response/ScheduleResultResponse.java
+++ src/main/java/com/test/scheduler/schedule/response/ScheduleResultResponse.java
@@ -1,2 +1,7 @@
-package com.test.scheduler.schedule.response;public interface ScheduleResultResponse {
+package com.test.scheduler.schedule.response;
+
+public interface ScheduleResultResponse {
+    public boolean isAgreeable();
+    public Integer getVotesYesQuant();
+    public Integer getVotesNoQuant();
 }
