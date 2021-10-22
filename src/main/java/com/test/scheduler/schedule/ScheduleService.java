cf593927406d66f229bd21d2ecc2e4490a7612a1
Valdery Alves <valdery.jur@gmail.com>
2021-10-22 03:21:43 -0300

    feat: Adds schedule service, saving, paginating and getting result report
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- src/main/java/com/test/scheduler/schedule/ScheduleService.java
+++ src/main/java/com/test/scheduler/schedule/ScheduleService.java
@@ -1,18 +1,29 @@
-package com.test.scheduler.service;
+package com.test.scheduler.schedule;
 
-import com.test.scheduler.model.ScheduleRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.test.scheduler.schedule.response.ScheduleResultResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ScheduleService {
 
-    private final ScheduleRepository repository;
+    @Autowired
+    private ScheduleRepository repository;
 
-    ScheduleService(ScheduleRepository repository) {
-        this.repository = repository;
+    public Schedule saveSchedule(Schedule newSchedule) {
+        return this.repository.save(newSchedule);
     }
 
-    ScheduleRepository saveSchedule(ScheduleRepository newSchedule) {
-        return this.repository.save(newSchedule);
+    public ScheduleResultResponse getScheduleResult (Integer scheduleId) {
+        return this.repository.getScheduleResult(scheduleId);
+    }
+
+    public Page<Schedule> findAllPaginated(Integer page) {
+        Pageable paginationConfig = PageRequest.of(page, 10);
+        return this.repository.findAll(paginationConfig);
     }
 
 }
