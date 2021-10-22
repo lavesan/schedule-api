558fa597ed1316e581885203cf053008ab039a7e
Valdery Alves <valdery.jur@gmail.com>
2021-10-22 03:25:39 -0300

    Adds the session controller
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- src/main/java/com/test/scheduler/session/SessionController.java
+++ src/main/java/com/test/scheduler/session/SessionController.java
@@ -1,2 +1,19 @@
-package com.test.scheduler.session;public class SessionController {
+package com.test.scheduler.session;
+
+import com.test.scheduler.session.request.SaveNewSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SessionController {
+
+    @Autowired
+    SessionService service;
+
+    @PostMapping("/session")
+    public Session saveNewSession(@RequestBody SaveNewSession body) {
+        return this.service.saveOne(body);
+    }
 }
