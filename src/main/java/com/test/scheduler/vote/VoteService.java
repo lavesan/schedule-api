4c093e825c8b6057916f8d40e560355cc0bde6ac
Valdery Alves <valdery.jur@gmail.com>
2021-10-22 03:28:14 -0300

    feat: Adds vote service with the method to create a new one and another to check if it can be added
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- src/main/java/com/test/scheduler/vote/VoteService.java
+++ src/main/java/com/test/scheduler/vote/VoteService.java
@@ -1,2 +1,41 @@
-package com.test.scheduler.vote;public class VoteService {
+package com.test.scheduler.vote;
+
+import com.test.scheduler.session.Session;
+import com.test.scheduler.vote.request.SaveNewVote;
+import com.test.scheduler.vote.response.CanVote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Service
+public class VoteService {
+
+    @Autowired
+    VoteRepository repository;
+
+    private boolean canVote(String cpf, Integer sessionId) {
+        return this.repository.hasVote(cpf, sessionId) == null;
+    }
+
+    public Object saveOne(SaveNewVote newVote) {
+        boolean canVote = this.canVote(newVote.cpf, newVote.sessionId);
+
+        if (!canVote) {
+            // TODO: Handler não pode votar
+            return null;
+        }
+
+        Vote voteToSave = new Vote();
+        voteToSave.setCpf(newVote.cpf);
+        voteToSave.setAgree(newVote.agree);
+
+        Session sessionToAdd = new Session();
+        sessionToAdd.setId(newVote.sessionId);
+
+        voteToSave.setSession(sessionToAdd);
+
+        return this.repository.save(voteToSave);
+    }
 }
