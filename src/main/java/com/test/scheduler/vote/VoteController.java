d36188f72fc4695cd9fe10d1ae21e8e86222b515
Valdery Alves <valdery.jur@gmail.com>
2021-10-22 03:28:27 -0300

    feat: Adds the vote controller
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- src/main/java/com/test/scheduler/vote/VoteController.java
+++ src/main/java/com/test/scheduler/vote/VoteController.java
@@ -1,2 +1,19 @@
-package com.test.scheduler.vote;public class VoteController {
+package com.test.scheduler.vote;
+
+import com.test.scheduler.vote.request.SaveNewVote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VoteController {
+
+    @Autowired
+    VoteService voteService;
+
+    @PostMapping("/vote")
+    public Object saveNewVote(@RequestBody SaveNewVote newVote) {
+        return this.voteService.saveOne(newVote);
+    }
 }
