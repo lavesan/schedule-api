f5e4d0f1088041cef4951edd0f17bbdb60c5ff10
Valdery Alves <valdery.jur@gmail.com>
2021-10-22 03:24:48 -0300

    feat: Adds the session service, with the method to create a new one
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- src/main/java/com/test/scheduler/session/SessionService.java
+++ src/main/java/com/test/scheduler/session/SessionService.java
@@ -1,2 +1,38 @@
-package com.test.scheduler.session;public class SessionService {
+package com.test.scheduler.session;
+
+import com.test.scheduler.schedule.Schedule;
+import com.test.scheduler.session.request.SaveNewSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+@Service
+public class SessionService {
+
+    @Autowired
+    SessionRepository repository;
+
+    public Session saveOne(SaveNewSession newSession) {
+        Date today = new Date();
+
+        if (newSession.activeTime == null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.MINUTE, 1);
+            newSession.activeTime = new Timestamp(calendar.getTimeInMillis());
+        }
+
+        Session sessionToSave = new Session();
+        sessionToSave.setStartedTime(new Timestamp(today.getTime()));
+        sessionToSave.setActiveTime(newSession.activeTime);
+
+        Schedule schedule = new Schedule();
+        schedule.setId(newSession.scheduleId);
+
+        sessionToSave.setSchedule(schedule);
+
+        return this.repository.save(sessionToSave);
+    }
 }
